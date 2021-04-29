46e2e149c8c95adb6eb5bc544a74a9c314bcb0fc
shahzadqasir2501 <59007872+shahzadqasir2501@users.noreply.github.com>
2021-04-29 20:18:26 +0500

    SetCleanderEvents

--- SEA_Application/Controllers/CalendarController.cs
+++ SEA_Application/Controllers/CalendarController.cs
@@ -21,58 +21,36 @@ namespace SEA_Application.Controllers
 
         public JsonResult GetEvents()
         {
-            var id = User.Identity.GetUserId();
-
-            //var events = from x in db.Events
-            //             where x.UserId == id
-            //             select new { _id = x.EventID, description = x.Description, end = x.End, allDay = x.IsFullDay, textColor = "#ffffff", title = x.Subject, backgroundColor = x.ThemeColor, start = TimeZoneInfo.ConvertTimeFromUtc(x.Start, TimeZoneInfo.Local), x.IsPublic, instructor = x.AspNetUser.Name, subjectClass = x.SubjectClass, x.Url, type = "Appointment", calendar = "Sales", LessonName = x.AspnetLesson.Name, className = x.AspnetLesson.AspnetSubjectTopic.AspnetGenericBranchClassSubject.AspNetClass.Name, Section = x.AspnetLesson.AspnetSubjectTopic.AspnetGenericBranchClassSubject.AspNetSection.Name };
-            var events = db.Events.Where(x => x.UserId == id).Select(x => new
-            { subjectName = x.AspnetLesson.AspnetSubjectTopic.GenericSubject.SubjectName, TimeTable = x.AspNetTimeTable.AspNetSubject.SubjectName ,_id = x.EventID, description = x.Description, end = x.End, allDay = x.IsFullDay, textColor = "#ffffff", title = x.Subject, backgroundColor = x.ThemeColor, start = x.Start, x.IsPublic, instructor = x.AspNetUser.Name, type = "Appointment", calendar = "Sales", LessonName = x.Subject, className = x.AspNetSession.SessionName, subjectClass = x.AspNetSession.SessionName }).ToList();
-
-            TimeZoneInfo PK_ZONE = TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time");
-            DateTime today = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.Date, PK_ZONE);
-            var eventstimatableList = new List<eventstimatable>();
-
-            foreach (var item in events)
+            try
             {
-                var eventstimatable = new eventstimatable();
-                eventstimatable.title = item.title;
-                if(item.subjectName == null)    { eventstimatable.className = "Old";  }
-                else  { eventstimatable.className = item.subjectClass; }
+                var id = User.Identity.GetUserId();
 
-                if (item.subjectName == null) { eventstimatable.subjectClass = "Old"; }
-                else { eventstimatable.subjectClass = item.subjectClass; }
+                //var events = from x in db.Events
+                //             where x.UserId == id
+                //             select new { _id = x.EventID, description = x.Description, end = x.End, allDay = x.IsFullDay, textColor = "#ffffff", title = x.Subject, backgroundColor = x.ThemeColor, start = TimeZoneInfo.ConvertTimeFromUtc(x.Start, TimeZoneInfo.Local), x.IsPublic, instructor = x.AspNetUser.Name, subjectClass = x.SubjectClass, x.Url, type = "Appointment", calendar = "Sales", LessonName = x.AspnetLesson.Name, className = x.AspnetLesson.AspnetSubjectTopic.AspnetGenericBranchClassSubject.AspNetClass.Name, Section = x.AspnetLesson.AspnetSubjectTopic.AspnetGenericBranchClassSubject.AspNetSection.Name };
 
-                eventstimatable.LessonName = item.LessonName;
-                eventstimatable.calendar = item.calendar;
-                eventstimatable.instructor = item.instructor;
-                eventstimatable._id = item._id;
-                eventstimatable.description = item.description;
-                eventstimatable.end = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(item.end.Value, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local);
-                eventstimatable.allDay = item.allDay;
-                eventstimatable.textColor = item.textColor;
-                eventstimatable.backgroundColor = item.backgroundColor;
-                eventstimatable.start = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(item.start, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local);
-                eventstimatable.IsPublic = item.IsPublic;
-                eventstimatableList.Add(eventstimatable);
-            }
+                var ThreeMonthBeforeDate = GetLocalDateTime.GetLocalDateTimeFunction().Value.AddMonths(-3);
 
-            if (User.IsInRole("Student"))
-            {
-                var eventstimatableStudentList = new List<eventstimatable>();
-                string subject = "";
+
+                var events = db.Events.Where(x => x.UserId == id && x.Start >= ThreeMonthBeforeDate).Select(x => new
+                { subjectName = x.AspnetLesson.AspnetSubjectTopic.GenericSubject.SubjectName, TimeTable = x.AspNetTimeTable.AspNetSubject.SubjectName, _id = x.EventID, description = x.Description, end = x.End, allDay = x.IsFullDay, textColor = "#ffffff", title = x.Subject, backgroundColor = x.ThemeColor, start = x.Start, x.IsPublic, instructor = x.AspNetUser.Name, type = "Appointment", calendar = "Sales", LessonName = x.Subject, className = x.AspNetSession.SessionName, subjectClass = x.AspNetSession.SessionName }).ToList();
+
+                TimeZoneInfo PK_ZONE = TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time");
+                DateTime today = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow.Date, PK_ZONE);
+                var eventstimatableList = new List<eventstimatable>();
+
                 foreach (var item in events)
                 {
                     var eventstimatable = new eventstimatable();
                     eventstimatable.title = item.title;
-                    //eventstimatable.SecTitle = item.SecTitle;
-                    //eventstimatable.Section = item.Section;
-                    if(item.subjectName == null) { subject = item.TimeTable; }
-                    else { subject = item.subjectName; }
-                    eventstimatable.className = subject;
+                    if (item.subjectName == null) { eventstimatable.className = "Old"; }
+                    else { eventstimatable.className = item.subjectClass; }
+
+                    if (item.subjectName == null) { eventstimatable.subjectClass = "Old"; }
+                    else { eventstimatable.subjectClass = item.subjectClass; }
+
                     eventstimatable.LessonName = item.LessonName;
                     eventstimatable.calendar = item.calendar;
-                    eventstimatable.subjectClass = subject;
                     eventstimatable.instructor = item.instructor;
                     eventstimatable._id = item._id;
                     eventstimatable.description = item.description;
@@ -82,13 +60,51 @@ namespace SEA_Application.Controllers
                     eventstimatable.backgroundColor = item.backgroundColor;
                     eventstimatable.start = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(item.start, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local);
                     eventstimatable.IsPublic = item.IsPublic;
-                    eventstimatableStudentList.Add(eventstimatable);
+                    eventstimatableList.Add(eventstimatable);
                 }
-                return new JsonResult { Data = eventstimatableStudentList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+                if (User.IsInRole("Student"))
+                {
+                    var eventstimatableStudentList = new List<eventstimatable>();
+                    string subject = "";
+                    foreach (var item in events)
+                    {
+                        var eventstimatable = new eventstimatable();
+                        eventstimatable.title = item.title;
+                        //eventstimatable.SecTitle = item.SecTitle;
+                        //eventstimatable.Section = item.Section;
+                        if (item.subjectName == null) { subject = item.TimeTable; }
+                        else { subject = item.subjectName; }
+                        eventstimatable.className = subject;
+                        eventstimatable.LessonName = item.LessonName;
+                        eventstimatable.calendar = item.calendar;
+                        eventstimatable.subjectClass = subject;
+                        eventstimatable.instructor = item.instructor;
+                        eventstimatable._id = item._id;
+                        eventstimatable.description = item.description;
+                        eventstimatable.end = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(item.end.Value, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local);
+                        eventstimatable.allDay = item.allDay;
+                        eventstimatable.textColor = item.textColor;
+                        eventstimatable.backgroundColor = item.backgroundColor;
+                        eventstimatable.start = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(item.start, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local);
+                        eventstimatable.IsPublic = item.IsPublic;
+                        eventstimatableStudentList.Add(eventstimatable);
+                    }
+                    return new JsonResult { Data = eventstimatableStudentList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                }
+
+                //var eventList = eventstimatableList.Select(x => new { title = x.title, className = x.className, LessonName = x.LessonName, calendar = x.calendar, subjectClass = x.subjectClass, instructor = x.instructor, _id = x._id, description = x.description, end = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(x.end.Value, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local), allDay = x.allDay, textColor = x.textColor, backgroundColor = x.backgroundColor, start = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(x.start, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local), IsPublic = x.IsPublic, });
+                return new JsonResult { Data = eventstimatableList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                var innerex = ex.InnerException.Message;
+                var inner = ex.InnerException;
             }
 
-            //var eventList = eventstimatableList.Select(x => new { title = x.title, className = x.className, LessonName = x.LessonName, calendar = x.calendar, subjectClass = x.subjectClass, instructor = x.instructor, _id = x._id, description = x.description, end = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(x.end.Value, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local), allDay = x.allDay, textColor = x.textColor, backgroundColor = x.backgroundColor, start = TimeZoneInfo.ConvertTimeFromUtc(TimeZoneInfo.ConvertTimeToUtc(x.start, TimeZoneInfo.FindSystemTimeZoneById("Pakistan Standard Time")), TimeZoneInfo.Local), IsPublic = x.IsPublic, });
-            return new JsonResult { Data = eventstimatableList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            return Json("", JsonRequestBehavior.AllowGet);
         }
 
         public class eventstimatable
